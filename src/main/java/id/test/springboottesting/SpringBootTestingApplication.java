b1f60bf27e4088bda5558a96f1891e7296fef063
mayur1064 <mayurtrap@gmail.com>
2021-07-23 10:08:29 +0530

    added tomcat

--- src/main/java/id/test/springboottesting/SpringBootTestingApplication.java
+++ src/main/java/id/test/springboottesting/SpringBootTestingApplication.java
@@ -1,14 +1,14 @@
 package id.test.springboottesting;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.boot.web.servlet.support.*;
+// import org.springframework.boot.*;
+// import org.springframework.boot.autoconfigure.*;
+// import org.springframework.boot.web.servlet.support.*;
 
-//import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+
 
 @SpringBootApplication
-public class SpringBootTestingApplication extends SpringBootServletInitializer {
+public class SpringBootTestingApplication {
 
 	public static void main(String[] args) {
 		 SpringApplication.run(SpringBootTestingApplication.class, args);
