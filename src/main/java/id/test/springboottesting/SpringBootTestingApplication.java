e08fc8ed117396d87cdd383a3bad036eb9722605
mayur1064 <mayurtrap@gmail.com>
2021-07-23 09:33:01 +0530

    added tomcat

--- src/main/java/id/test/springboottesting/SpringBootTestingApplication.java
+++ src/main/java/id/test/springboottesting/SpringBootTestingApplication.java
@@ -1,5 +1,9 @@
 package id.test.springboottesting;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.web.servlet.support.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
