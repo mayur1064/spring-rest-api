6366f0180ced04542b975fe1810f06d1da444b75
mayur1064 <mayurtrap@gmail.com>
2021-07-26 10:27:51 +0530

    readme

--- src/main/java/id/test/springboottesting/controller/UserController.java
+++ src/main/java/id/test/springboottesting/controller/UserController.java
@@ -69,15 +69,15 @@ public class UserController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/api2/{id}")
-    public ResponseEntity<User> updateUser2(@PathVariable Long id, @RequestBody @Valid User user) {
-        return userService.findUserById(id)
-                .map(userObj -> {
-                    userObj.setId(id);
-                    return ResponseEntity.ok(userService.updateUser(userObj));
-                })
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
+    // @PutMapping("/api2/{id}")
+    // public ResponseEntity<User> updateUser2(@PathVariable Long id, @RequestBody @Valid User user) {
+    //     return userService.findUserById(id)
+    //             .map(userObj -> {
+    //                 userObj.setId(id);
+    //                 return ResponseEntity.ok(userService.updateUser(userObj));
+    //             })
+    //             .orElseGet(() -> ResponseEntity.notFound().build());
+    // }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable Long id) {
